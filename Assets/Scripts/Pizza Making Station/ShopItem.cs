6c28f9a4a008942e4397b784a94196ba6921e964
spacegirl298 <jessicajardim2004@gmail.com>
2024-06-17 19:01:55 +0200

    DONE BITCH

--- Assets/Scripts/Pizza Making Station/ShopItem.cs
+++ Assets/Scripts/Pizza Making Station/ShopItem.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Shop/Shop Item")]
+//[CreateAssetMenu(menuName = "Shop/Shop Item")]
 public class ShopItem : MonoBehaviour
 {
     public Sprite sprite; 
